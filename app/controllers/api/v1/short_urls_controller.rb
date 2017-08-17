6271ba2145083aa9c2d58040746966f505c3cd06
Aaron Kyle Cuizon <aaron.kyle.cuizon@gmail.com>
2017-08-18 01:21:36 +0800

    added api controller

---
+++ app/controllers/api/v1/short_urls_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::ShortUrlsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    url = ShortUrl.new(original_url: params[:url])
+    
+    if url.save
+      render json: url, status: 200
+    else
+      render json: { errors: url.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    url = ShortUrl.find_by(slug: params[:id])
+
+    if url.present?
+      render json: url, status: 200
+    else
+      render json: { errors: "ShortUrl not found." }, status: 404
+    end
+  end
+
+end
