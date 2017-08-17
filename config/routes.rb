b740863f03e332f5839a95214790c9274784d020
Aaron Kyle Cuizon <aaron.kyle.cuizon@gmail.com>
2017-08-18 01:26:49 +0800

    yield :scripts in application layout; added route to api

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,17 @@ Rails.application.routes.draw do
 
   root to: "short_urls#index"
 
-  resources :short_urls, only: :create do
+  resources :short_urls, only: [] do
     member do 
       get '/stats', to: "short_urls#stats"
+    end
+  end
+
+  get "/:id", to: "short_urls#show"
+
+  namespace :api do
+    namespace :v1 do
+      resources :short_urls, only: [:create, :show]
     end
   end
 
