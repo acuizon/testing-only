ed494b3170727ff3d2fb72620e099021fbdac4c1
Aaron Kyle Cuizon <aaron.kyle.cuizon@gmail.com>
2017-08-18 01:40:40 +0800

    callback to add protocol to original_url and generate string; add validates format to original_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,5 +2,21 @@ class ShortUrl < ApplicationRecord
 
   validates :original_url, :slug, presence: true
   validates :slug, uniqueness: true
+  validates :original_url, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }
+
+  before_validation :process, on: :create
+
+  private
+
+  def process
+    if !self.original_url[/\A(http|https):\/\//i]
+      self.original_url = "http://#{self.original_url}"
+    end
+
+    loop do
+      possible_slug = SecureRandom.urlsafe_base64(10)
+      break self.slug = possible_slug unless ShortUrl.find_by(slug: possible_slug).present?
+    end
+  end
 
 end
